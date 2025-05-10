a72ae6ad87c1402e644632a22dae87f3b3dd33c2
Norihiro Kumagai <tendai22plus@gmail.com>
2025-05-10 17:00:10 +0900

    out-8bit: VScode pico-sdk sample template project created

---
+++ out-8bit/out-8bit.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/uart.h"
+
+#include "blink.pio.h"
+
+#define CLK_Pin  40
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d kHz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (125000 / (2 * freq)) - 3;
+}
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 46
+#define UART_RX_PIN 47
+
+static int toggle_value = 1;
+#define TOGGLE() do {    gpio_xor_mask64(((uint64_t)1)<<TEST_Pin); } while(0)
+
+void gpio_out_init(uint gpio, bool value) {
+    gpio_set_dir(gpio, GPIO_OUT);
+    gpio_put(gpio, value);
+    gpio_set_function(gpio, GPIO_FUNC_SIO);
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
+    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+
+    // PIO Blinking example
+    PIO pio = pio0;
+    pio_set_gpio_base(pio, 16);
+    uint offset = pio_add_program(pio, &blink_program);
+    printf("Loaded program at %d\n", offset);
+    
+    blink_pin_forever(pio, 0, offset, CLK_Pin, 3);
+    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+    
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
+    
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
